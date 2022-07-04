57505b7d38096162e8fa2f7e466c6625fb915cf0
shohan <shohan889@yahoo.com>
2022-07-05 00:32:21 +0600

    Folder structured

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
-function App() {
+
+export default ()=> {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2 className="bg-red-500"> From app</h2>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Route path="about" element={<About />} /> */}
+      </Routes>
+
     </div>
   );
 }
 
-export default App;
+
---
+++ src/components/home/ProductList.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default () => {
+  return (
+    <>
+      <h2 style={{ marginTop: "500px" }} className="bg-gray-200  text-red-500">
+        Product list ht dfsdf
+      </h2>
+      <p className="bg-gray-200  text-red-500">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit esse
+        molestiae odio voluptates, beatae praesentium iste quo? Cum voluptate
+        quos eum suscipit saepe, officia necessitatibus.
+      </p>
+    </>
+  );
+};
